bc63a2d921fc64bb858883c9f4c4eb38a90d738f
kongning <kongningemail@163.com>
2017-12-15 14:22:52 +0800

    nothing changed here

--- src/test/java/GainTestData.java
+++ src/test/java/GainTestData.java
@@ -8,7 +8,7 @@ public class GainTestData {
     public static void main(String... args) throws IOException {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
         System.out.println(simpleDateFormat.format(new Date()));
-        GenerateTestData generateTestData = new GenerateTestData(1, 1, 1);
+        GenerateTestData generateTestData = new GenerateTestData(20, 20, 20);
         generateTestData.gainData(10000 * 15000);
         System.out.println(simpleDateFormat.format(new Date()));
     }
